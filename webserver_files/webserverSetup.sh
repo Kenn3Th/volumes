529399f19fb964aba4c16a604c9b16e23c7f6998
Kenn3Th <kenneth.eikrehagen@gmail.com>
2020-05-14 18:38:14 +0000

    Adding webserver files

---
+++ webserver_files/webserverSetup.sh
@@ -0,0 +1,34 @@
+printf "Starting setup for web-servers\n"
+
+echo "Making web1"
+docker run -d \
+--publish 8081:80 \
+--name web1 \
+-v ~/volumes/web1:/var/www/html \
+-v ~/volumes/webserver_files/web1.com:/etc/nginx/sites-available/web1.com \
+richarvey/nginx-php-fpm:latest
+
+echo "Making web2"
+docker run -d \
+--publish 8082:80 \
+--name web2 \
+-v ~/volumes/web2:/var/www/html \
+-v ~/volumes/webserver_files/web2.com:/etc/nginx/sites-available/web2.com \
+richarvey/nginx-php-fpm:latest
+
+echo "Making web3"
+docker run -d \
+--publish 8083:80 \
+--name web3 \
+-v ~/volumes/web3/html:/var/www/html \
+-v ~/volumes/webserver_files/web3.com:/etc/nginx/sites-available/web3.com \
+richarvey/nginx-php-fpm:latest
+
+sleep 5
+
+echo "Making lb"
+docker run -d \
+--publish 80:80 \
+--name lb \
+-v ~/volumes/lb/:/usr/local/etc \
+haproxy:latest
