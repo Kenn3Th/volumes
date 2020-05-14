72e9adb5452186d5e693927f5fbe58f18f234d4d
Kenneth R. Eikrehagen <45165531+Kenn3Th@users.noreply.github.com>
2020-05-14 23:29:27 +0200

    Update webserverSetup.sh

--- setup/webserverSetup.sh
+++ setup/webserverSetup.sh
@@ -1,7 +1,7 @@
 printf "Starting setup for web-servers\n"
 
 echo "Making web1"
-docker run -d \
+sudo docker run -d \
 --publish 8081:80 \
 --name web1 \
 -v ~/volumes/web1:/var/www/html \
@@ -9,7 +9,7 @@ docker run -d \
 richarvey/nginx-php-fpm:latest
 
 echo "Making web2"
-docker run -d \
+sudo docker run -d \
 --publish 8082:80 \
 --name web2 \
 -v ~/volumes/web2:/var/www/html \
@@ -17,7 +17,7 @@ docker run -d \
 richarvey/nginx-php-fpm:latest
 
 echo "Making web3"
-docker run -d \
+sudo docker run -d \
 --publish 8083:80 \
 --name web3 \
 -v ~/volumes/web3/html:/var/www/html \
@@ -27,7 +27,7 @@ richarvey/nginx-php-fpm:latest
 sleep 5
 
 echo "Making lb"
-docker run -d \
+sudo docker run -d \
 --publish 80:80 \
 --name lb \
 -v ~/volumes/lb:/usr/local/etc \
