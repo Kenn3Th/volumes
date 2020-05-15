11a41615821b7fe1e5d305faba203034ffcc6aaa
Kenneth R. Eikrehagen <45165531+Kenn3Th@users.noreply.github.com>
2020-05-15 13:09:47 +0200

    Update parameters.sh

--- setup/parameters.sh
+++ setup/parameters.sh
@@ -1,29 +1,28 @@
 #Change this IP's to the IP range your network has
-export web1_ip="172.17.0.2"
-export web2_ip="172.17.0.3"
-export web3_ip="172.17.0.4"
-export lb_ip="172.17.0.5"
-export db1_ip="172.17.0.6"
-export db2_ip="172.17.0.7"
-export db3_ip="172.17.0.8"
-export maxscale_ip="172.17.0.9"
+web1_ip="172.17.0.2"
+web2_ip="172.17.0.3"
+web3_ip="172.17.0.4"
+lb_ip="172.17.0.5"
+db1_ip="172.17.0.6"
+db2_ip="172.17.0.7"
+db3_ip="172.17.0.8"
+maxscale_ip="172.17.0.9"
 #Password and username section
-export DATS_USERNAME="dats44"
-export MYSQL_USER="maxscaleuser" \
-export MYSQL_ROOT_PASSWORD="rootpass" \
-export MAXSCALEUSER_PASSWORD="maxscalepass" \
-export MAXSCALEUSER_USER="maxscaleuser" \
-export MYSQL_PASSWORD="sure caught drop" \
+DATS_USERNAME="dats44"
+MYSQL_ROOT_PASSWORD="rootpass" \
+MAXSCALEUSER_PASSWORD="maxscalepass" \
+MAXSCALEUSER_USER="maxscaleuser" \
+MYSQL_PASSWORD="sure caught drop" \
 
-#export web1_ip
-#export web2_ip
-#export web3_ip
-#export lb_ip
-#export db1_ip
-#export db2_ip
-#export db3_ip
-#export maxscale_ip
-#export MYSQL_USER
-#export DATS_USERNAME
-#export MYSQL_ROOT_PASSWORD
-#export MYSQL_PASSWORD
+export web1_ip
+export web2_ip
+export web3_ip
+export lb_ip
+export db1_ip
+export db2_ip
+export db3_ip
+export maxscale_ip
+export DATS_USERNAME
+export MAXSCALEUSER_PASSWORD
+export MYSQL_ROOT_PASSWORD
+export MYSQL_PASSWORD
